6595e250e597c75320126da459d390f83e00b188
rcgriffin <robert.griffioen@student.hu.nl>
2020-01-13 11:36:23 +0100

    domain structure changed

--- generator/src/main/java/hu/tosad2019/groep4/generator/domain/businessrule/attributerangerule/AttributeRangeRule.java
+++ generator/src/main/java/hu/tosad2019/groep4/generator/domain/businessrule/attributerangerule/AttributeRangeRule.java
@@ -1,82 +1,43 @@
 package hu.tosad2019.groep4.generator.domain.businessrule.attributerangerule;
 
-import hu.tosad2019.groep4.generator.domain.Enums.Operator;
+import hu.tosad2019.groep4.generator.domain.Column;
+import hu.tosad2019.groep4.generator.domain.Range;
+import hu.tosad2019.groep4.generator.domain.businessrule.BaseRule;
+import hu.tosad2019.groep4.generator.domain.businessrule.BusinessRule;
 
-public class AttributeRangeRule {
-	
-	private String triggerName;
-	private String tableName;	
-	private String columnName;
-	
+public class AttributeRangeRule implements BusinessRule {
+
+	private BaseRule rule;
+
+	private Column column;
+
+	private boolean not;
+
 	private boolean forEachRow;
-	
-	private int position;
-	private int minValue;
-	private int maxValue;
-	
-	private Operator minValueOperator;
-	private Operator maxValueOperator;
-	private Operator andOrOperator;
-	
-	private AttributeRangeRule() { }
-	
-	public static class Builder {
-		private AttributeRangeRule instance;
-		public Builder() { instance = new AttributeRangeRule(); }
-		
-		public Builder setTriggerName(String triggerName) {
-			instance.triggerName = triggerName;
-			return this;
-		}
-		
-		public Builder setTableName(String tableName) {
-			instance.tableName = tableName;
-			return this;
-		}
-		
-		public Builder setPosition(int position) {
-			instance.position = position;
-			return this;
-		}
-		
-		public Builder setforEachRow(boolean forEachRow) {
-			instance.forEachRow = forEachRow;
-			return this;
-		}		
-		
-		public Builder setColumnName(String columnName) {
-			instance.columnName = columnName;
-			return this;
-		}
-		
-		public Builder setMinValue(int minValue) {
-			instance.minValue = minValue;
-			return this;
-		}
-		
-		public Builder setMaxValue(int maxValue) {
-			instance.maxValue = maxValue;
-			return this;
-		}
-		
-		public Builder setMinValueOperator(Operator operator) {
-			instance.minValueOperator = operator;
-			return this;
-		}
-		
-		public Builder setMaxValueOperator(Operator operator) {
-			instance.maxValueOperator = operator;
-			return this;
-		}
-		
-		public Builder setAndOrOperator(Operator operator) {
-			instance.andOrOperator = operator;
-			return this;
-		}
-		
-		public AttributeRangeRule build() {
-			return instance;
-		}
+
+	private Range range;
+
+	private AttributeRangeRule(BaseRule baseRule, AttributeRangeRuleContext context) {
+		this.rule = baseRule;
+		this.column = context.getColumn();
+		this.not = context.getNot();
+		this.forEachRow = context.getForEachRow();
+		this.range = context.getRange();
+	}
+
+	@Override
+	public String getCode() {
+		return rule.getCode();
+	}
+
+	@Override
+	public String getName() {
+		return rule.getName();
+	}
+
+	@Override
+	public String getDescription() {
+		return rule.getDescription();
 	}
 
 }
