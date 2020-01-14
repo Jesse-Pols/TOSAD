43588681d1ae1f970a87297c5c534c404ab93f12
Jesse Pols <j.pols@infoplaza.nl>
2020-01-14 14:03:02 +0100

    hibernate default

--- generator/src/main/java/hu/tosad2019/groep4/generator/Main.java
+++ generator/src/main/java/hu/tosad2019/groep4/generator/Main.java
@@ -8,13 +8,27 @@ import hu.tosad2019.groep4.generator.domain.objects.Range;
 import hu.tosad2019.groep4.generator.domain.objects.businessrule.BusinessRule;
 import hu.tosad2019.groep4.generator.domain.objects.businessrule.attributerangerule.AttributeRangeRule;
 import hu.tosad2019.groep4.generator.domain.objects.businessrule.attributerangerule.AttributeRangeRuleContext;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Main
 {
 	public static void main(String[] args) {
+
+
+
+		Configuration con = new Configuration();
+
+		SessionFactory sf = con.buildSessionFactory();
+
+		Session session = sf.openSession();
+		session.save(null);
+
 		/*
 		 * TEMPLATES BABYYY
 		 */
+		/*
 		String templateName = "ATTRIBUTE_COMPARE_RULE";
 		HashMap<String, String> variables = new HashMap<String, String>();
 		variables.put("trigger_name", "trigger_1");
@@ -22,6 +36,8 @@ public class Main
 		variables.put("operator", "AND");
 		String replacedtemplate = TemplateParser.parse(templateName, variables);
 		System.out.println(replacedtemplate);
+
+		 */
 		/*
 		 * EINDE
 		 */
@@ -36,14 +52,14 @@ public class Main
 			e.printStackTrace();
 		}
 		*/
-
+/*
 		Range range = new Range(0, 10, Operator.and);
 
 		AttributeRangeRuleContext rangeRuleContext = new AttributeRangeRuleContext(true, 0, range);
 
 		BusinessRule rangeRule = new AttributeRangeRule("rr1", "rangerule 1", "dit is een testobject", rangeRuleContext);
 
-
+*/
 
 	}
 }
