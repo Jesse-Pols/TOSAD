fbddc5915939117686dfea38bd6d3d75cfdb63aa
rcgriffin <robert.griffioen@student.hu.nl>
2020-01-14 12:13:43 +0100

    component interface toegevoegd voor processing

---
+++ designer/src/main/java/hu/tosad2019/groep4/designer/domain/processing/ObjectService.java
@@ -0,0 +1,14 @@
+package hu.tosad2019.groep4.designer.domain.processing;
+
+import hu.tosad2019.groep4.designer.domain.objects.businessrule.attributecomparerule.AttributeCompareRule;
+import hu.tosad2019.groep4.designer.domain.objects.businessrule.attributerangerule.AttributeRangeRule;
+
+public class ObjectService {
+    public AttributeCompareRule getAttributeCompareRule(){
+        return null;
+    }
+
+    public AttributeRangeRule getAttributeRangeRule(){
+        return null;
+    }
+}
