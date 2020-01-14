196055a085242ae978280b67f392633b8f5dcaf1
maartenverheul <maarten.verheul@student.hu.nl>
2020-01-14 12:26:27 +0100

    Removed template execution snippet from Main

--- generator/src/main/java/hu/tosad2019/groep4/generator/Main.java
+++ generator/src/main/java/hu/tosad2019/groep4/generator/Main.java
@@ -1,13 +1,11 @@
 package hu.tosad2019.groep4.generator;
 
-import java.util.HashMap;
-
-import hu.tosad2019.groep4.generator.domain.objects.businessrule.attributerangerule.*;
-import hu.tosad2019.groep4.generator.domain.objects.Enums.Operator;
 import hu.tosad2019.groep4.generator.domain.objects.Range;
+import hu.tosad2019.groep4.generator.domain.objects.Enums.Operator;
 import hu.tosad2019.groep4.generator.domain.objects.businessrule.BusinessRule;
 import hu.tosad2019.groep4.generator.domain.objects.businessrule.attributerangerule.AttributeRangeRule;
 import hu.tosad2019.groep4.generator.domain.objects.businessrule.attributerangerule.AttributeRangeRuleContext;
+import hu.tosad2019.groep4.generator.server.ServerListener;
 
 public class Main
 {
@@ -15,19 +13,18 @@ public class Main
 		/*
 		 * TEMPLATES BABYYY
 		 */
-		String templateName = "ATTRIBUTE_COMPARE_RULE";
-		HashMap<String, String> variables = new HashMap<String, String>();
-		variables.put("trigger_name", "trigger_1");
-		variables.put("position", "BEFORE");
-		variables.put("operator", "AND");
-		String replacedtemplate = TemplateParser.parse(templateName, variables);
-		System.out.println(replacedtemplate);
+//		String templateName = "ATTRIBUTE_COMPARE_RULE";
+//		HashMap<String, String> variables = new HashMap<String, String>();
+//		variables.put("trigger_name", "trigger_1");
+//		variables.put("position", "BEFORE");
+//		variables.put("operator", "AND");
+//		String replacedtemplate = TemplateParser.parse(templateName, variables);
+//		System.out.println(replacedtemplate);
 		/*
 		 * EINDE
 		 */
 
 
-		/*
 		ServerListener server;
 		try {
 			server = ServerListener.create(8000);
@@ -35,7 +32,6 @@ public class Main
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		*/
 
 		Range range = new Range(0, 10, Operator.and);
 
