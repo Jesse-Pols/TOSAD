1bed0403b2e815847d13ee6aebccc79a9f12359a
rcgriffin <robert.griffioen@student.hu.nl>
2020-01-15 11:31:05 +0100

    added application component interface, defined facades, removed unnecessary facades

---
+++ designer/src/main/java/hu/tosad2019/groep4/designer/application/DefineRule.java
@@ -0,0 +1,78 @@
+package hu.tosad2019.groep4.designer.application;
+
+import hu.tosad2019.groep4.designer.domain.objects.businessrule.BusinessRule;
+import hu.tosad2019.groep4.designer.domain.processing.BusinessRuleFactory;
+import hu.tosad2019.groep4.designer.domain.processing.ObjectService;
+import hu.tosad2019.groep4.designer.domain.processing.enums.BusinessRuleType;
+
+import java.util.HashMap;
+
+public class DefineRule {
+    public static boolean saveBusinessRule(String typeString, HashMap<String, String> properties) {
+        boolean success = false;
+        BusinessRuleType type = null;
+        BusinessRule rule = null;
+
+
+
+
+        switch (typeString){
+            case "AttributeCompareRule":
+                type = BusinessRuleType.AttributeCompareRule;
+                break;
+            case "AttributeRangeRule":
+                type = BusinessRuleType.AttributeRangeRule;
+                break;
+            default:
+                break;
+        }
+
+
+
+        BusinessRuleFactory factory = new BusinessRuleFactory(type, properties);
+        rule = factory.MakeBusinessRule();
+
+        ObjectService objectService = new ObjectService();
+
+        return  objectService.saveBusinessRule(rule);
+    }
+
+    public static HashMap<String, String> getProperties(String BusinessRuleType){
+
+        HashMap<String, String> properties = new HashMap<>();
+
+        switch (BusinessRuleType){
+            case "AttributeRangeRule":
+                properties = getRangeRuleProperties();
+                break;
+            case "AttributeCompareRule":
+                properties = getCompareRuleProperties();
+                break;
+            default:
+                properties = null;
+        }
+
+
+        return properties;
+    }
+
+
+
+    private static HashMap<String, String> getRangeRuleProperties(){
+        HashMap<String, String> properties = new HashMap<String, String>();
+
+        properties.put("", "");
+
+
+        return properties;
+    }
+
+    private static HashMap<String, String> getCompareRuleProperties(){
+        HashMap<String, String> properties = new HashMap<String, String>();
+
+        properties.put("", "");
+
+
+        return properties;
+    }
+}
--- designer/src/main/java/hu/tosad2019/groep4/designer/application/ManageRuleFacade.java
+++ designer/src/main/java/hu/tosad2019/groep4/designer/application/ManageRuleFacade.java
@@ -2,65 +2,22 @@ package hu.tosad2019.groep4.designer.application;
 
 import java.util.HashMap;
 
+//          application component interface
 public class ManageRuleFacade {
 
-    public static boolean saveBusinessRule(String type, HashMap<String, String> properties) {
-        boolean success = false;
-
-        switch (type){
-            default:
-                success = false;;
-        }
-
-        return success;
-    }
-
-    public static boolean updateBusinessRule(String code, HashMap<String, String>properties){
-        return false;
-    }
-
+    //code, name
     public static HashMap<String, String> getAllBusinessRules(){
         return null;
     }
 
+    public static boolean saveBusinessRule(String type, HashMap<String, String> properties) {
+        return DefineRule.saveBusinessRule(type, properties);
+    }
 
-    public  HashMap<String, String> getProperties(String BusinessRuleType){
-
-        HashMap<String, String> properties = new HashMap<>();
-
-        switch (BusinessRuleType){
-            case "AttributeRangeRule":
-                properties = getRangeRuleProperties();
-                break;
-            case "AttributeCompareRule":
-                properties = getCompareRuleProperties();
-                break;
-            default:
-                properties = null;
-        }
-
-
-        return properties;
+    public  HashMap<String, String> getProperties(String businessRuleType){
+        return DefineRule.getProperties(businessRuleType);
     }
 
 
 
-
-    private HashMap<String, String> getRangeRuleProperties(){
-        HashMap<String, String> properties = new HashMap<String, String>();
-
-        properties.put("", "");
-
-
-        return properties;
-    }
-
-    private HashMap<String, String> getCompareRuleProperties(){
-        HashMap<String, String> properties = new HashMap<String, String>();
-
-        properties.put("", "");
-
-
-        return properties;
-    }
 }
