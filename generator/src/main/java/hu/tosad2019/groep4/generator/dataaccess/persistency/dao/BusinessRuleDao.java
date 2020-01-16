358d8d8cb86a42bb4b6ab14000aa8cc79eb9e161
Jesse Pols <j.pols@infoplaza.nl>
2020-01-16 09:43:54 +0100

    Added Businessrule Dao

--- generator/src/main/java/hu/tosad2019/groep4/generator/dataaccess/persistency/dao/BusinessRuleDao.java
+++ generator/src/main/java/hu/tosad2019/groep4/generator/dataaccess/persistency/dao/BusinessRuleDao.java
@@ -1,4 +1,17 @@
 package hu.tosad2019.groep4.generator.dataaccess.persistency.dao;
 
-public class BusinessRuleDao {
+import hu.tosad2019.groep4.generator.dataaccess.objects.dataaccesobjects.BusinessRule;
+import hu.tosad2019.groep4.generator.dataaccess.objects.dataaccesobjects.Range;
+import hu.tosad2019.groep4.generator.dataaccess.persistency.IBusinessRuleDao;
+import hu.tosad2019.groep4.generator.datainfrastructure.BaseDao;
+import org.hibernate.Session;
+
+public class BusinessRuleDao extends BaseDao implements IBusinessRuleDao {
+    public BusinessRule getById(int id) {
+        Session session = super.sessionFactory.openSession();
+        BusinessRule businessRule = session.get(BusinessRule.class, id);
+
+        session.close();
+        return businessRule;
+    }
 }
