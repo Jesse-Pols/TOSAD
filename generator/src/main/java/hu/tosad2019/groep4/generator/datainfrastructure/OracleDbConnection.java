553b415a327e2ba76aee1eb7b6cc9c0c284dfdb6
rcgriffin <robert.griffioen@student.hu.nl>
2020-01-16 14:26:24 +0100

    added DBconnection objects in infrastructure package for executing SQL on target databases

---
+++ generator/src/main/java/hu/tosad2019/groep4/generator/datainfrastructure/OracleDbConnection.java
@@ -0,0 +1,24 @@
+package hu.tosad2019.groep4.generator.datainfrastructure;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class OracleDbConnection implements DbConnection {
+    private final String baseConnectionString = "jdbc:oracle:@";
+    private String connectionString = "jbdc:oracle:thin@localhost:1521:xe";
+
+    public OracleDbConnection(String hostName){
+        this.connectionString = baseConnectionString + hostName + ":1521:xe";
+    }
+
+
+    public Connection getConnection(String userName, String password) throws SQLException {
+        return DriverManager.getConnection(connectionString, userName, password);
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return getConnection("robert", "Oracle4Life");
+    }
+}
