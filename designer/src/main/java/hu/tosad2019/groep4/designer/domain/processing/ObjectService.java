a95722477ccff8cf5a95a1e99af9dacc9f5270de
Jesse Pols <j.pols@infoplaza.nl>
2020-01-20 12:35:47 +0100

    fixed error, IBusinessRule was still being used

--- designer/src/main/java/hu/tosad2019/groep4/designer/domain/processing/ObjectService.java
+++ designer/src/main/java/hu/tosad2019/groep4/designer/domain/processing/ObjectService.java
@@ -1,14 +1,12 @@
 package hu.tosad2019.groep4.designer.domain.processing;
 
-import hu.tosad2019.groep4.designer.application.storage.objects.domainobjects.IBusinessRule;
-import hu.tosad2019.groep4.designer.application.storage.PersistencyService;
 import hu.tosad2019.groep4.designer.domain.processing.enums.BusinessRuleType;
 
 import java.util.HashMap;
 
 public class ObjectService {
-    public IBusinessRule getBusinessRule(BusinessRuleType type, HashMap<String, String> attributes){
-        return new BusinessRuleFactory(type, attributes).MakeBusinessRule();
+    public void getBusinessRule(BusinessRuleType type, HashMap<String, String> attributes){
+        //return new BusinessRuleFactory(type, attributes).MakeBusinessRule();
     }
 
     public boolean saveBusinessRule(){
