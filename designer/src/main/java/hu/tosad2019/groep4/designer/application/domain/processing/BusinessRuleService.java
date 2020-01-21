660d4383fb2c594964d3db1d9e907335cdac1b64
rcgriffin <robert.griffioen@student.hu.nl>
2020-01-21 14:13:31 +0100

    expanded domain component interface

--- designer/src/main/java/hu/tosad2019/groep4/designer/application/domain/processing/BusinessRuleService.java
+++ designer/src/main/java/hu/tosad2019/groep4/designer/application/domain/processing/BusinessRuleService.java
@@ -2,16 +2,24 @@ package hu.tosad2019.groep4.designer.application.domain.processing;
 
 import hu.tosad2019.groep4.designer.application.domain.objects.businessrule.BusinessRule;
 import hu.tosad2019.groep4.designer.application.domain.processing.enums.BusinessRuleType;
+import hu.tosad2019.groep4.designer.application.storage.PersistencyService;
 
 import java.util.HashMap;
 
 public class BusinessRuleService {
-    public void getBusinessRule(BusinessRuleType type, HashMap<String, String> attributes){
+    private PersistencyService persistencyService;
+
+    public void getBusinessRule(BusinessRuleContext context){
+        this.persistencyService = new PersistencyService();
+
         //return new BusinessRuleFactory(type, attributes).MakeBusinessRule();
     }
 
     public boolean saveBusinessRule(BusinessRule rule){
-        return true;
+        return false;
     }
 
+    public boolean deleteBusinessRule(BusinessRule rule){
+        return false;
+    }
 }
