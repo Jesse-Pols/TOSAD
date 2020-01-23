b18041fae1928939c4392eab186b9115c9715b99
Jesse Pols <j.pols@infoplaza.nl>
2020-01-23 15:22:16 +0100

    stashed code

--- designer/src/main/java/hu/tosad2019/groep4/designer/application/storage/AbstractPersistency.java
+++ designer/src/main/java/hu/tosad2019/groep4/designer/application/storage/AbstractPersistency.java
@@ -3,9 +3,7 @@ package hu.tosad2019.groep4.designer.application.storage;
 import hu.tosad2019.groep4.designer.application.domain.processing.BusinessRuleContext;
 import hu.tosad2019.groep4.designer.application.domain.processing.enums.BusinessRuleType;
 import hu.tosad2019.groep4.designer.application.storage.dao.*;
-import hu.tosad2019.groep4.designer.application.storage.objects.BusinessRuleModel;
-import hu.tosad2019.groep4.designer.application.storage.objects.BusinessRuleTypeModel;
-import hu.tosad2019.groep4.designer.application.storage.objects.DbColumnModel;
+import hu.tosad2019.groep4.designer.application.storage.objects.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,19 +15,57 @@ public class AbstractPersistency {
     protected BusinessRuleCategoryDao businessRuleCategoryDao = new BusinessRuleCategoryDao();
     protected DbColumnDao dbColumnDao = new DbColumnDao();
     protected TemplateDao templateDao = new TemplateDao();
+    protected StatementDao statementDao = new StatementDao();
+    protected RangeDao rangeDao = new RangeDao();
+    protected SpecifiedValueDao specifiedValueDao = new SpecifiedValueDao();
+    protected ListDao listDao = new ListDao();
 
     protected BusinessRuleContext convertIdToContext(int id) {
 
+        /* MODELS */
         BusinessRuleModel businessRuleModel = businessRuleDao.find(id);
-        //BusinessRuleTypeModel businessRuleTypeModel = businessRuleModel.getType();
-        //List<DbColumnModel> dbColumns = dbColumnDao.findByRuleId(id);
 
-        //BusinessRuleContext context = this.getCorrectType(businessRuleTypeModel.getName());
-        BusinessRuleContext context = new BusinessRuleContext(BusinessRuleType.AttributeRangeRule);
+
+
+
+
+        BusinessRuleTypeModel businessRuleTypeModel = businessRuleModel.getType();
+        BusinessRuleCategoryModel businessRuleCategoryModel = businessRuleTypeModel.getCategory();
+        TemplateModel templateModel = businessRuleTypeModel.getTemplate();
+
+        //List<StatementModel> statementModels = statementDao.findAllByRuleId(id);
+        //StatementModel statementModel = statementModels.get(0);
+
+
+        /* CONTEXT */
+        BusinessRuleContext context = this.getCorrectType(businessRuleTypeModel.getName());
+
+        // Business Rule
         context.setName(businessRuleModel.getName());
         context.setId(businessRuleModel.getId());
-        //context.setDescription(businessRuleModel.getDescription());
-        //context.setFailure(businessRuleModel.getFailure());
+        context.setDescription(businessRuleModel.getDescription());
+        context.setFailure(businessRuleModel.getFailure());
+        context.setIsNot(businessRuleModel.getIsNot());
+
+        // Business Type
+        context.setTypeId(businessRuleModel.getTypeId());
+
+        // Business Category
+        context.setCategory(businessRuleCategoryModel.getName());
+        context.setCategoryId(businessRuleCategoryModel.getId());
+
+        // Template
+        context.setTemplate(templateModel.getValue());
+        context.setTemplateId(templateModel.getId());
+
+        // Statement
+        //context.setStatement(statementModel.getStatement());
+        //context.setStatementId(statementModel.getId());
+
+        // Range
+        //context.setMaxValue(rangeModel.getMaxValue());
+        //context.setMinValue(rangeModel.getMinValue());
+
 
         /*
         for (DbColumnModel column : dbColumns) {
