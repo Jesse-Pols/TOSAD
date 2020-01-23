c629d2dcbeaea7c993e1aa589865d8c852866390
rcgriffin <robert.griffioen@student.hu.nl>
2020-01-23 10:09:34 +0100

    renamed objectservice to businessruleservice

--- generator/src/main/java/hu/tosad2019/groep4/generator/application/domain/processing/BusinessRuleService.java
+++ generator/src/main/java/hu/tosad2019/groep4/generator/application/domain/processing/BusinessRuleService.java
@@ -3,10 +3,10 @@ package hu.tosad2019.groep4.generator.application.domain.processing;
 import hu.tosad2019.groep4.generator.application.storage.PersistencyService;
 import hu.tosad2019.groep4.generator.application.domain.objects.businessrule.BusinessRule;
 
-public class ObjectService {
+public class BusinessRuleService {
     PersistencyService persistencyService = new PersistencyService();
 
-    public BusinessRule getBusinessRule(int id){
+    public BusinessRule getRule(int id){
         BusinessRule businessRule = persistencyService.getBusinessRuleById(id);
         return businessRule;
     }
