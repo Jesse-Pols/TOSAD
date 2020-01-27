e3d84249c356fc376e472ac71a507c71dddeeaad
walid <wavzaly@gmail.com>
2020-01-27 10:23:42 +0100

    Updated business rule objects

--- designer/src/main/java/hu/tosad2019/groep4/designer/application/domain/objects/businessrule/attributelistrule/AttributeListRule.java
+++ designer/src/main/java/hu/tosad2019/groep4/designer/application/domain/objects/businessrule/attributelistrule/AttributeListRule.java
@@ -1,9 +1,26 @@
 package hu.tosad2019.groep4.designer.application.domain.objects.businessrule.attributelistrule;
 
+import hu.tosad2019.groep4.designer.application.domain.objects.Column;
+import hu.tosad2019.groep4.designer.application.domain.objects.SpecifiedValue;
 import hu.tosad2019.groep4.designer.application.domain.objects.businessrule.BaseRule;
+import hu.tosad2019.groep4.designer.application.domain.objects.enums.Operator;
+
+import java.util.List;
 
 public class AttributeListRule extends BaseRule {
-    public AttributeListRule(String name, String description) {
+    private boolean not;
+    private Column column;
+    private Operator operator;
+    private List<SpecifiedValue> list;
+
+
+    public AttributeListRule(String name, String description, AttributeListRuleContext context) {
         super(name, description);
+        this.not = context.getNot();
+        this.column = context.getColumn();
+        this.operator = context.getOperator();
+        this.list = context.getSpecifiedValuelist();
     }
 }
+
+
