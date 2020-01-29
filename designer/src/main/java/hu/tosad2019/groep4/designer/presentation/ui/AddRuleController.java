613f548ceff8439559002400924a710a4a5f6bfb
bartvantongeren <bart.vantongern@student.hu.nl>
2020-01-29 13:10:44 +0100

    Added searchbar

--- designer/src/main/java/hu/tosad2019/groep4/designer/presentation/ui/AddRuleController.java
+++ designer/src/main/java/hu/tosad2019/groep4/designer/presentation/ui/AddRuleController.java
@@ -27,6 +27,8 @@ public class AddRuleController {
 	@FXML private Button btn_save;
 	@FXML private TextField messageBox;
 	@FXML private Label alert_message;
+	@FXML private TextField searchTextField;
+	@FXML private Button searchBtn;
 
 	private Map<String, Object> currentProperties = new LinkedHashMap<>();
 	private List<BusinessRuleType> rules = List.of(BusinessRuleType.values());
@@ -178,5 +180,10 @@ public class AddRuleController {
 	}
 
 	public void hideAlertMessage() { this.alert_message.setTextFill(Color.web("#212121")); }
+
+	@FXML private void search_onclick() {
+
+	}
+
 
 }
