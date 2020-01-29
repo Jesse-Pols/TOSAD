b624ff7e82813f0728c38c2fae34bc77297cc28a
rcgriffin <robert.griffioen@student.hu.nl>
2020-01-29 16:39:34 +0100

    renamed targetDbConnection to targetDbContext adde targetDbController to test db conneciton

--- designer/src/main/java/hu/tosad2019/groep4/designer/presentation/ui/MainController.java
+++ designer/src/main/java/hu/tosad2019/groep4/designer/presentation/ui/MainController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import hu.tosad2019.groep4.designer.application.application.MainFacade;
-import hu.tosad2019.groep4.designer.application.application.TargetDbConnection;
+import hu.tosad2019.groep4.designer.application.application.TargetDbContext;
 import hu.tosad2019.groep4.designer.application.domain.objects.Column;
 import hu.tosad2019.groep4.designer.application.domain.objects.SpecifiedValue;
 import hu.tosad2019.groep4.designer.application.domain.objects.businessrule.BusinessRule;
@@ -62,7 +62,7 @@ public class MainController {
 
 	private ObservableList<BusinessRule> rules;
 
-	private TargetDbConnection targetDbConnection;
+	private TargetDbContext targetDbContext;
 
 	@FXML private void initialize() {
 
@@ -209,7 +209,7 @@ public class MainController {
 
 		this.sendError("TODO: Connect");
 		
-		this.targetDbConnection = new TargetDbConnection(type, host, port_nr, username, password);
+		this.targetDbContext = new TargetDbContext(type, host, port_nr, username, password);
 	}
 
 	@FXML
@@ -263,7 +263,7 @@ public class MainController {
 	}
 
 	private void generateBusinessRule(int id) throws Exception {
-		MainFacade.getInstance().generateBusinessRule(id, targetDbConnection);
+		MainFacade.getInstance().generateBusinessRule(id, targetDbContext);
 	}
 	private List<BusinessRule> getBusinessrules(){
 		List<BusinessRule> rules = new ArrayList<>();
